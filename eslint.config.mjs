f6955ce25943c3f7ec202f0c8ab09b14fa35bc11
Icaro Almeida <progalmeidaregis@outlook.com>
2026-08-13 16:15:09 -0300

    feat: implementalção do husky + lintstaged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
